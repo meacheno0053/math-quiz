a658f200ea437d08cb9baaf00e471e865eab6204
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-07-01 00:27:56 +0000

    deleted the unnecessary max guesses component as it is not needed

---
+++ 05_Rounds_mechanics.py
@@ -0,0 +1,55 @@
+# Round numbers for each individual equation
+
+
+def check_rounds():
+    while true:
+        response = input("How many rounds:")
+
+        round_error = "Please type either <Enter> " \
+                    "or an integer that is more than 0"
+        if response != "":
+            try:
+                response = int(response)
+
+                if response <1:
+                    print(round_error)
+                    continue
+            except ValueError:
+                print(round_error)
+                continue
+
+
+                
+    return response
+
+        
+Rounds_played = 0
+choose_instruction = "Please answer the question"
+
+# Ask user for number of rounds, or <enter> for infinate
+rounds = check_rounds()
+
+end_game = "no"
+while end_game == "no":
+
+
+    print()
+    if rounds == "":
+        heading = "Continuous Mode: Round {}".format(rounds_played +1)
+        print(heading)
+    print(heading)
+    choose = input("{} or 'xxx' to end:".format(choose_instruction))
+    if choose == 'xxx':
+        break
+    else:
+        heading = "round {} of {}".format(rounds_played + 1, rounds)
+        print(heading)
+        choose = input(choose_instruction)
+        if Rounds_played == rounds - 1:
+            end_game = "yes"
+
+
+    print("You chose {}".format(choose))
+
+    Rounds_played += 1
+
