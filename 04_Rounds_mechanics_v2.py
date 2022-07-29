40a14ba35a3e4a1815c4964f568d3f51ed59594d
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-07-29 00:29:11 +0000

    the rounds mechanics have been edited again and are now completely functional and work as expected

--- 04_Rounds_mechanics_v2.py
+++ 04_Rounds_mechanics_v2.py
@@ -1,32 +1,31 @@
-# Main routine is more efficient that v2
+# Main routine is more efficient that v1
 
 
 def check_rounds():
   while True:
-    response = input ("How many rounds: ")
-
-    round_error = "Please typr either <Enter>, xxx " \
-                  "or an integer that is more than 0"
+    response = input ("How many rounds? : ")
+    
+    round_error = "Please type either <Enter>, xxx " \
+    "or an integer that is more than 0"
     if response != "":
-      try:
-        response = int(response)
-
-        if response <1:
-          print (round_error)
-          continue
-
-      except ValueError:
-        print(round_error)
-        continue
-
-  return response
+        try:
+            response = int(response)
+            
+            if response <1:
+                print (round_error)
+                continue
+            
+        except ValueError:
+            print(round_error)
+            continue
+    
+    return response
 
 
 # Main routine goes here...
 
 rounds_played = 0 
-choose_instruction = "Please choose rock (r), paper " \
-                     "(p) or scissors (s)"
+choose_instruction = "< INSERT ANSWER HERE >"
 
 # Ask user for # of rounds, <enter> for infinate mode
 
@@ -43,7 +42,7 @@ while end_game =="no":
   if rounds =="":
     heading = "Continuous Mode: Round {}".format(rounds_played +1)
   else:
-    heading = "Round {} of".format(rounds_played + 1, rounds)    
+    heading = "Round {} of {}".format(rounds_played + 1, rounds)    
 
   
   print(heading)
@@ -54,7 +53,7 @@ while end_game =="no":
     break
 
   # **** rest of loop / game ****
-  print("You chose {}".format(choose))
+  print("Your answer is {}".format(choose))
 
   rounds_played += 1
 
@@ -63,4 +62,5 @@ while end_game =="no":
     break
 
 # Put end game content here
+print()
 print("Thank you for playing")
