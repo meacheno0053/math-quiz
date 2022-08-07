5624f823697034c79f294c51fa2a04a38ead6e92
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-08-07 23:03:56 +0000

    got the rounds working and the questions randomly generating, need to fix exit code

--- 00_sandpit_2.0.py
+++ 00_sandpit_2.0.py
@@ -5,7 +5,7 @@ import random
 
 def check_rounds():
   while True:
-    response = input ("How many rounds? : ")
+    response = input ("How many Questions? : ")
     
     round_error = "Please type either <Enter>, xxx " \
     "or an integer that is more than 0"
@@ -27,7 +27,7 @@ def check_rounds():
 # Main routine goes here...
 
 rounds_played = 0 
-choose_instruction = "< INSERT ANSWER HERE >"
+
 
 # Ask user for # of rounds, <enter> for infinate mode
 
@@ -42,38 +42,35 @@ while end_game =="no":
     # Rounds Heading
     print()
     if rounds =="":
-        heading = "Continuous Mode: Round {}".format(rounds_played +1)
+        heading = "Continuous Mode: Question {}".format(rounds_played +1)
     else:
-        heading = "Round {} of {}".format(rounds_played + 1, rounds)    
+        heading = "Question {} of {}".format(rounds_played + 1, rounds)    
     
-    
-        print(heading)
-    
-    
-    
-        choose = input("{} or 'xxx' to end: ".format(choose_instruction))
-    
+    print(heading)
+
+
+
     # End game if exit code is typed
-    if choose == "xxx":
-        break
+
+
+
+    first = random.randint(1, 100)
+    second = random.randint(1, 100)
+    answer = first + second
+
+    question = "{} + {} = ".format(first, second)
+    print(question)
     
-    
-        first = random.randint(1, 100)
-        second = random.randint(1, 100)
-        answer = first + second
-    
-        question = "{} + {} = ".format(first, second)
-        print(question)
-    
-        run = True
+    run = True
     while run :
         user_input = int(input("Your answer: "))
-    if user_input == answer:
-        print("correct!")
-        print("Program continues")
-        run = False
-    elif user_input < answer:
-        print("Incorrect! The correct answer is:", answer)
+        if user_input == answer:
+            print("correct!")
+            run = False
+        elif user_input < answer:
+            print("Incorrect! The correct answer is:", answer)
+        if user_input < answer:
+         break
     
     rounds_played += 1
     
